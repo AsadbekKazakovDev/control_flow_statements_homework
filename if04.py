6510b745e5b7ea8421819fe74b8042bdac516f11
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-23 08:45:40 +0500

    4-masala yechimi

--- if04.py
+++ if04.py
@@ -8,4 +8,17 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    k=0
+    if a>0 and b>0 and c>0:
+        k+=3
+    if (a>0 and b>0 and c<0) or (a>0 and b<0 and c>0) or (a<0 and b>0 and c>0):
+        k+=2
+    if (a>0 and b<0 and c<0) or (a<0 and b<0 and c>0) or (a<0 and b>0 and c<0):
+        k+=1
+    return k
+a,b,c = 3,2,-1
+print(main(a,b,c))
+a,b,c = 4,-2,-6
+print(main(a,b,c))
+a,b,c = 1,7,9
+print(main(a,b,c))
