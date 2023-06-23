afc2046f154ccdfd0b6431d4811840d5a800f299
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-23 10:21:08 +0500

    10-masala yechimi

--- if10.py
+++ if10.py
@@ -13,4 +13,27 @@ def main(temp):
     Returns:
         string: the message to print
     """
-    return
+    s=""
+    if temp<0:
+        s="Freezing"
+    if temp>0 and temp<10:
+        s="Very Cold"
+    if temp>11 and temp<=20:
+        s="Cold"
+    if temp>21  and temp<30:
+        s="Normal"
+    if temp>31 and temp<40:
+        s="Hot"
+    if temp>40:
+        s="Very Hot"
+    return s
+a = 23
+print(main(a))
+a=56
+print(main(a))
+a=12
+print(main(a))
+a=8
+print(main(a))
+a=-9
+print(main(a))
