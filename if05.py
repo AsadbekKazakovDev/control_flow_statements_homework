7d362809c50affb37baa3d0a21409ebada6ce7ec
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-23 08:52:40 +0500

    5-masala yechimi

--- if05.py
+++ if05.py
@@ -8,4 +8,21 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    k = 0
+    if a>0 and b>0 and c>0:
+        k+=0
+    if (a<0 and b>0 and c>0) or (a>0 and b<0 and c>0) or (a>0 and b>0 and c<0):
+        k+=1
+    if (a<0 and b<0 and c>0)or(a>0 and b<0 and c<0)or(a<0 and b>0 and c<0):
+        k+=2
+    if a<0 and b<0 and c<0:
+        k+=3
+    return k
+a,b,c = 3 , 4 , -9
+print(main(a,b,c))
+a,b,c = -3,-9,8
+print(main(a,b,c))
+a,b,c = -8,-49,-78
+print(main(a,b,c))
+a,b,c = 9,3,6
+print(main(a,b,c))
