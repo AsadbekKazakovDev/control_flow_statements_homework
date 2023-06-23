9c2737a53ea1048aa4c3089cd79d16024fd7ad39
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-23 10:09:46 +0500

    9-masala yechimi

--- if09.py
+++ if09.py
@@ -9,4 +9,16 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    b = a//10
+    c = a%10
+    k = False
+    j = c*10+b
+    if j==a:
+        k=True  
+    return k
+a = 45
+print(main(a))
+a = 33
+print(main(a))
+a = 43
+print(main(a))
